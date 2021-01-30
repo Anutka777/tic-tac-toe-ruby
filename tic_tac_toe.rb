73416c0b0d5a817334cbd3d99c1856f709e34fe3
Anna Baburina <4nna.baburina@gmail.com>
2021-01-30 09:11:01 +0500

    ADD User input check for board cell

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -37,22 +37,32 @@ class Board
     puts '-------------'
   end
 
-  def self.ask_and_return_mark_place
+  def self.ask_mark_place
     puts 'Where do you want to put a mark?'
-    puts 'Enter the number of the cell.'
-    @choice = gets
+    puts 'Enter the number of a cell you want to occupy.'
+    place = gets.chomp!.to_i
+    until place >= 1 && place <= 9
+      puts 'Pick a number of the cell from 1 to 9'
+      place = gets.chomp!.to_i
+    end
+    return place
+  end
+
+  def self.put_mark(mark = '.')
+    @choice = Board.ask_mark_place
+    p @choice
   end
 end
 
-puts 'Player 1, what is your name?'
-p1 = Players.new(gets)
-puts p1.name
-puts 'Player 2, what is your name?'
-p2 = Players.new(gets)
-puts p2.name
+# puts 'Player 1, what is your name?'
+# p1 = Players.new(gets.chomp!)
+# puts "Hello, #{p1.name}! You will be playing with X mark"
+# puts 'Player 2, what is your name?'
+# p2 = Players.new(gets.chomp!)
+# puts "Hello, #{p2.name}! You will be playing with 0 mark"
 
 board = Board.new
 board.draw_board
 
 Board.new
-puts Board.ask_and_return_mark_place
+Board.put_mark
