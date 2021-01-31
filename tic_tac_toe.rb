a0e170dd7dbfef58d57ad3093bc8cc9bad246869
Anna Baburina <4nna.baburina@gmail.com>
2021-01-31 07:41:38 +0500

    Game class methods

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,14 +2,30 @@
 
 # Tic-tac-toe
 class Game
+  def self.ask_players_names
+    puts 'Player 1, what is your name?'
+    player1 = gets.chomp!
+    Players.new(player1, 'X')
+    puts "Hello, #{player1}!\nYou will be playing with X mark"
+    puts 'Player 2, what is your name?'
+    player2 = gets.chomp!
+    Players.new(player2, 'O')
+    puts "Hello, #{player2}!\nYou will be playing with 0 mark"
+  end
+
+  def initialize(player1, player2)
+    @player_x = player1
+    @player_o = player2
+  end
 end
 
 # Players
 class Players
   attr_reader :name
 
-  def initialize(name)
+  def initialize(name, mark)
     @name = name
+    @mark = mark
   end
 end
 
@@ -37,32 +53,27 @@ class Board
     puts '-------------'
   end
 
-  def self.ask_mark_place
+  def self.ask_and_validate_mark_placement
     puts 'Where do you want to put a mark?'
     puts 'Enter the number of a cell you want to occupy.'
-    place = gets.chomp!.to_i
-    until place >= 1 && place <= 9
+    @place = gets.chomp!.to_i
+    until @place >= 1 && @place <= 9
       puts 'Pick a number of the cell from 1 to 9'
-      place = gets.chomp!.to_i
+      @place = gets.chomp!.to_i
     end
-    return place
+    return @place
   end
 
   def self.put_mark(mark = '.')
-    @choice = Board.ask_mark_place
-    p @choice
+    @cell_choice = Board.ask_and_validate_mark_placement
+    p @cell_choice
   end
 end
 
-# puts 'Player 1, what is your name?'
-# p1 = Players.new(gets.chomp!)
-# puts "Hello, #{p1.name}! You will be playing with X mark"
-# puts 'Player 2, what is your name?'
-# p2 = Players.new(gets.chomp!)
-# puts "Hello, #{p2.name}! You will be playing with 0 mark"
+# board = Board.new
+# board.draw_board
 
-board = Board.new
-board.draw_board
+# Board.new
+# Board.put_mark
 
-Board.new
-Board.put_mark
+Game.ask_players_names
