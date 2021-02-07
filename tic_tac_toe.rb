52f4390be65c310306f38b8ce6e309380ad83b44
Anna Baburina <4nna.baburina@gmail.com>
2021-02-07 21:47:24 +0500

    REF Simplify project structure

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,29 +2,16 @@
 
 # Tic-tac-toe
 class Game
-  def self.ask_players_names
-    puts 'Player 1, what is your name?'
-    player1 = gets.chomp!
-    Players.new(player1, 'X')
-    puts "Hello, #{player1}!\nYou will be playing with X mark"
-    puts 'Player 2, what is your name?'
-    player2 = gets.chomp!
-    Players.new(player2, 'O')
-    puts "Hello, #{player2}!\nYou will be playing with 0 mark"
-  end
-
-  def initialize(player1, player2)
-    @player_x = player1
-    @player_o = player2
+  def initialize
+    @player_x = Players.new('X')
+    @player_o = Players.new('0')
+    @board = Board.new
   end
 end
 
 # Players
 class Players
-  attr_reader :name
-
-  def initialize(name, mark)
-    @name = name
+  def initialize(mark)
     @mark = mark
   end
 end
@@ -32,48 +19,43 @@ end
 # Board
 class Board
   def initialize
-    @cell1 = 1
-    @cell2 = 2
-    @cell3 = 3
-    @cell4 = 4
-    @cell5 = 5
-    @cell6 = 6
-    @cell7 = 7
-    @cell8 = 8
-    @cell9 = 9
+    @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
-  def draw_board
-    puts '-------------'
-    puts "| #{@cell1} | #{@cell2} | #{@cell3} |"
-    puts '-------------'
-    puts "| #{@cell4} | #{@cell5} | #{@cell6} |"
-    puts '-------------'
-    puts "| #{@cell7} | #{@cell8} | #{@cell9} |"
-    puts '-------------'
+  def print_on_screen
+    puts <<~HEREDOC
+      -------------
+      | #{@cells[0]} | #{@cells[1]} | #{@cells[2]} |
+      -------------
+      | #{@cells[3]} | #{@cells[4]} | #{@cells[5]} |
+      -------------
+      | #{@cells[6]} | #{@cells[7]} | #{@cells[8]} |
+    HEREDOC
   end
 
-  def self.ask_and_validate_mark_placement
-    puts 'Where do you want to put a mark?'
-    puts 'Enter the number of a cell you want to occupy.'
-    @place = gets.chomp!.to_i
-    until @place >= 1 && @place <= 9
-      puts 'Pick a number of the cell from 1 to 9'
-      @place = gets.chomp!.to_i
-    end
-    return @place
-  end
-
-  def self.put_mark(mark = '.')
-    @cell_choice = Board.ask_and_validate_mark_placement
-    p @cell_choice
+  def put_mark(cell, mark)
+    cells[cell - 1] = mark
   end
 end
 
-# board = Board.new
-# board.draw_board
+# def self.ask_players_names
+#   puts 'Player 1, what is your name?'
+#   player1 = gets.chomp!
+#   Players.new(player1, 'X')
+#   puts "Hello, #{player1}!\nYou will be playing with X mark"
+#   puts 'Player 2, what is your name?'
+#   player2 = gets.chomp!
+#   Players.new(player2, 'O')
+#   puts "Hello, #{player2}!\nYou will be playing with 0 mark"
+# end
 
-# Board.new
-# Board.put_mark
-
-Game.ask_players_names
+# def self.ask_and_validate_mark_placement
+#   puts 'Where do you want to put a mark?'
+#   puts 'Enter the number of a cell you want to occupy.'
+#   @place = gets.chomp!.to_i
+#   until @place >= 1 && @place <= 9
+#     puts 'Pick a number of the cell from 1 to 9'
+#     @place = gets.chomp!.to_i
+#   end
+#   return @place
+# end
