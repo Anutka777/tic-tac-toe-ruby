5863dd32bc23aee27007f58dfba355fe498f25f6
Anna Baburina <4nna.baburina@gmail.com>
2021-02-14 10:56:33 +0500

    ADD occupancy_check and control_game_flow methods

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,37 +5,54 @@ class Game
   attr_reader :player_x, :player_o, :board
 
   def initialize
-    @player_x = Players.new('X')
-    @player_o = Players.new('0')
+    @player_x = Players.new('Player1', 'X')
+    @player_o = Players.new('Player2', '0')
     @board = Board.new
   end
 
-  def control_game_flow; end
+  def control_game_flow
+    board.print_on_screen
+    loop do
+      print "#{player_x.name}, please enter the number of the cell you want to place your mark.\n"
+      board.place_mark(player_x.mark)
+      board.print_on_screen
+      break if check_winnier == true
+      print "#{player_o.name}, please enter the number of the cell you want to place your mark.\n"
+      board.place_mark(player_o.mark)
+      board.print_on_screen
+      break if check_winnier == true
+    end
+    game_over
+  end
 
-  def determine_winnier; end
+  def check_winnier
+    true
+  end
 
-  def game_over; end
+  def game_over
+    puts 'Thanks for playing.'
+  end
 end
 
 # To make a move
 class Players
-  def initialize(mark)
+  attr_reader :name, :mark
+
+  def initialize(name, mark)
+    @name = name
     @mark = mark
   end
 
   # Returns input
-  def make_move
-    print "Please enter the number of the cell you want to place your mark.\n"
+  def self.make_move
     loop do
-      puts 'You should enter the number from 1 to 9.'
+      print '(Enter the number from 1 to 9) > '
       input = gets.chomp.to_i
       break input if validate_input(input) == true
     end
   end
 
-  private
-
-  def validate_input(input)
+  def self.validate_input(input)
     input >= 1 && input <= 9 ? true : false
   end
 end
@@ -54,11 +71,22 @@ class Board
       | #{@cells[3]} | #{@cells[4]} | #{@cells[5]} |
       -------------
       | #{@cells[6]} | #{@cells[7]} | #{@cells[8]} |
+      -------------
     HEREDOC
   end
 
-  def place_mark(cell, mark)
-    @cells[cell - 1] = mark
+  def place_mark(mark)
+    loop do
+      puts 'Make sure the cell is not occupaid'
+      cell = Players.make_move
+      break @cells[cell - 1] = mark if occupancy_check(cell) == true
+    end
+  end
+
+  private
+
+  def occupancy_check(cell)
+    @cells[cell - 1].is_a? Integer
   end
 end
 
@@ -73,22 +101,13 @@ end
 #   puts "Hello, #{player2}!\nYou will be playing with 0 mark"
 # end
 
-# def self.ask_and_validate_mark_placement
-#   puts 'Where do you want to put a mark?'
-#   puts 'Enter the number of a cell you want to occupy.'
-#   @place = gets.chomp!.to_i
-#   until @place >= 1 && @place <= 9
-#     puts 'Pick a number of the cell from 1 to 9'
-#     @place = gets.chomp!.to_i
-#   end
-#   return @place
-# end
-
 new_game = Game.new
-p new_game.player_o
-p new_game.player_x.make_move
+# p new_game.player_o
+# p new_game.player_x.make_move
 # p new_game.board
-# new_game.board.place_mark(1, 'X')
+# new_game.board.place_mark(new_game.player_x.make_move, new_game.player_x.mark)
 # new_game.board.place_mark(19, 'O')
 # new_game.board.print_on_screen
 # p new_game.player_x.validate_input(22.to_i)
+new_game.control_game_flow
+# p Players.make_move
